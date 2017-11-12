bfed875b8700a94bb18013a1df75f1ec0dbc0717
Andrew Fawcett <andrewfawcett@Sarahs-MacBook.local>
2017-11-12 10:51:06 -0800

    Added code to disconnect from CometD when the component is destroyed

--- src/aura/streaming/streamingController.js
+++ src/aura/streaming/streamingController.js
@@ -3,15 +3,16 @@
         var action = component.get("c.getSessionId");
         action.setCallback(this, function(response) {
         
-            // Configure CometD
+            // Configure CometD for this component
             var sessionId = response.getReturnValue();
             var cometd = new window.org.cometd.CometD();
             cometd.configure({
-                url: window.location.protocol + '//' + window.location.hostname + '/cometd/40.0/',
+                url: window.location.protocol + '//' + window.location.hostname + '/cometd/41.0/',
                 requestHeaders: { Authorization: 'OAuth ' + sessionId},
                 appendMessageTypeToURL : false
             });
             cometd.websocketEnabled = false;
+            component.set('v.cometd', cometd);
 
             // Connect to 
             cometd.handshake($A.getCallback(function(status) {
@@ -19,19 +20,31 @@
                     var eventName = component.get("v.channel");
                     cometd.subscribe(eventName, $A.getCallback(function(message) {
                             var messageEvent = component.getEvent("onMessage");
-                            if(messageEvent!=null) {
+	                        if(messageEvent!=null) {
                                 messageEvent.setParam("payload", message.data.payload);
-                                messageEvent.fire();
-                            }
+                                messageEvent.fire();                            
+	                        }
                         }
                     ));
                 } else {
-                    // TODO: Handle errors 
-                    console.log(status);
+                    // TODO: Throw an event / set error property here?
+                    console.log('streaming component: ' + status);
                 }
             }));
 
         });
         $A.enqueueAction(action);
+    },
+    handleDestroy : function (component, event, helper) {
+        // Ensure this component disconnects from the server
+		var cometd = component.get("v.cometd");
+        cometd.disconnect(function(disconnectReply) 
+			{ 
+                if(disconnectReply.successful) {
+                    console.log('streaming component: Success disconnect')
+                } else {
+                    console.log('streaming component: Failed disconnect')                    
+                }
+            });
     }
 })
