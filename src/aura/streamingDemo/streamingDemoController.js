c79588a7cdd9f1d74d328459ee3cc0b97312d68d
Andrew Fawcett <andrewfawcett@Sarahs-MacBook.local>
2017-11-12 11:15:55 -0800

    Improvements to the demo component

--- src/aura/streamingDemo/streamingDemoController.js
+++ src/aura/streamingDemo/streamingDemoController.js
@@ -1,5 +1,16 @@
 ({
-    handleMessage : function(component, event, helper) {
-        component.set("v.lastMessagePayload", JSON.stringify(event.getParam("payload")));
-    }
+	handleMessage : function(cmp, event, helper) {
+    	var payload = event.getParam("payload");
+        $A.createComponent(
+            "aura:html", { "tag" : "p", "body" : JSON.stringify(payload), 
+            HTMLAttributes: { "class" : "slds-p-vertical_xx-small" }},
+            function(newMessage, status, errorMessage){
+                if (status === "SUCCESS") {
+                    var body = cmp.get("v.body");
+                    body.unshift(newMessage);
+                    cmp.set("v.body", body);
+                }
+            }
+        );        		
+	}
 })
